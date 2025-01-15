e71620d6a05d502ced38334fa89fe51bc15cfe63
Mufaro1410 <fungurayimufaro@gmail.com>
2025-01-15 12:54:04 +0200

    added dark and light theme

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+export const Colors = {
+    light: {
+        text: 'black',
+        background: '#fff',
+        icon: 'black',
+        button: 'royalblue'
+    },
+    dark: {
+        text: 'white',
+        background: '#000',
+        icon: 'white',
+        button: 'white'
+    }
+}
