b900ddfe2d307cd3a1a29ffcf63f0e3861add1c2
Mufaro1410 <fungurayimufaro@gmail.com>
2025-01-18 21:15:16 +0200

    added styles to dynamic page

--- app/todos/[id].jsx
+++ app/todos/[id].jsx
@@ -43,6 +43,8 @@ export default function EditScreen() {
         return null
     }
 
+    const styles = createStyles(theme, colorScheme)
+
     const handleSave = async () => {
         try {
             const savedTodo = { ...todo, title: todo.title}
@@ -64,16 +66,78 @@ export default function EditScreen() {
     }
 
     return (
-        <SafeAreaView>
-            <View>
-                <TextInput />
-                <Pressable></Pressable>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    maxLength={30}
+                    placeholder="Edit todo"
+                    placeholderTextColor='gray'
+                    value={todo?.title || ''}
+                    onChangeText={(text) => setTodo(prev => ({...prev, title: text}))}
+                />
+                <Pressable
+                    onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style={{ marginLeft: 10 }}
+                >
+                    <Octicons name={colorScheme === 'dark' ? "moon" : "sun"} size={36} color={theme.text} selectable={undefined} style={{ width: 36 }} />
+                </Pressable>
             </View>
-            <View>
-                <Pressable></Pressable>
-                <Pressable></Pressable>
+            <View style={styles.inputContainer}>
+                <Pressable
+                    onPress={handleSave}
+                    style={styles.saveButton}
+                >
+                    <Text styles={styles.saveButtonText}>Save</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => router.push('/')}
+                    style={[styles.saveButton, { backgroundColor: 'red' }]}
+                >
+                    <Text styles={[styles.saveButtonText, { color: 'white' }]}>Cancel</Text>
+                </Pressable>
             </View>
             <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'}/>
         </SafeAreaView>
     )
+}
+
+function createStyles(theme, colorScheme) {
+    return StyleSheet.create({
+        container: {
+            flex: 1,
+            width: '100%',
+            backgroundColor: theme.background,
+        },
+        inputContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            padding: 10,
+            gap: 6,
+            width: '100%',
+            maxWidth: 1024,
+            marginHorizontal: 'auto',
+            pointerEvents: 'auto',
+        },
+        input: {
+            flex: 1,
+            borderColor: 'gray',
+            borderWidth: 1,
+            borderRadius: 5,
+            padding: 10,
+            marginRight: 10,
+            fontSize: 18,
+            fontFamily: 'Inter_500Medium',
+            minWidth: 0,
+            color: theme.text,
+        },
+        saveButton: {
+            backgroundColor: theme.button,
+            borderRadius: 5,
+            padding: 10,
+        },
+        saveButtonText: {
+            fontSize: 18,
+            color: colorScheme === 'dark' ? 'black' : 'white'
+        }
+    })
 }
